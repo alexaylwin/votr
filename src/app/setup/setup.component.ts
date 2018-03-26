f169056c50f7bfe831eadb60f2ca07b0eae4f4ad
Alex <AlexAylwin@gmail.com>
2018-03-26 16:47:20 -0400

    Added markup for game setup screen

--- src/app/setup/setup.component.ts
+++ src/app/setup/setup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-setup',
+  selector: 'votr-setup',
   templateUrl: './setup.component.html',
   styleUrls: ['./setup.component.scss']
 })
